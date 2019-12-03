40019d968ed3823fdc13dbbe85865b6e593b636b
wangjian <1934623103@qq.com>
2019-12-03 18:54:55 +0800

    拷贝一份静态页面模板，修改了springboot 访问资源目录的方法。

--- src/main/java/com/wangjian/Controller/JspIndexController.java
+++ src/main/java/com/wangjian/Controller/JspIndexController.java
@@ -15,4 +15,39 @@ public class JspIndexController {
         logger.info("now we at jspIndex");
         return "jspIndex";
     }
+    @RequestMapping("/index")
+    public String Index(){
+        logger.info("now we at Index");
+        return "index";
+    }
+
+    @RequestMapping("/icon-fontawesome")
+    public String iconFontawesome(){
+        logger.info("now we at icon-fontawesome");
+        return "icon-fontawesome";
+    }
+
+    @RequestMapping("/pages-blank")
+    public String pagesBlank(){
+        logger.info("now we at pages-blank");
+        return "pages-blank";
+    }
+
+    @RequestMapping("/pages-error-404")
+    public String pagesError404(){
+        logger.info("now we at pages-error-404");
+        return "pages-error-404";
+    }
+
+    @RequestMapping("/pages-profile")
+    public String pagesProfile(){
+        logger.info("now we at pages-profile");
+        return "pages-profile";
+    }
+
+    @RequestMapping("/table-basic")
+    public String tableBasic(){
+        logger.info("now we at table-basic");
+        return "table-basic";
+    }
 }
